b441020e6e5ad6971010e778e5b0e42fda86daa7
laughseejapan <laughseejapan@gmail.com>
2022-02-14 03:38:43 +0800

    first commit

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '登錄賬號與密碼不匹配，請再次嘗試，或點選 忘記密碼',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
